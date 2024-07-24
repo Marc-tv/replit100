93924e297ea80b576d9d58cb8ec59e2186b8a5df
Marc-tv <marcvatapilly@gmail.com>
2024-07-24 19:07:51 +0530

    Create fay6.py

---
+++ fay6.py
@@ -0,0 +1,11 @@
+username = input("what is your loggin name ")
+if username == "marc":
+  print("Ah! welcome back Marc!.")
+elif username == "ron":
+  print("Long time no see Ron .")
+elif username == "jack":
+  print("its good to have an old friend back. Enjoy jack!")
+elif username == "luke":
+      print("cant wait to see how much you've improved, Luke .")
+else: 
+  print("I don't remember you. Please try again.")
