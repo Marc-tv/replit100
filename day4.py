eaf29f52cd60bf3001e8cafbaa9a705c1fd7f33f
Marc-tv <marcvatapilly@gmail.com>
2024-07-24 18:52:32 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,6 @@
+print("welcome player 1 ")
+username = input("what is your username: ")
+power = input("what would you like as a special ability ?: ")
+origin = input("where do you originate from ?: ")
+enemy = input("who isyour enemy?: ")
+print("Welcome", username, "this is a world of war, will your powers off", power, "be able to help you conquere this world and take everything that it has to offer. Do you think a man from", origin, "will be able to complete this quest.", enemy,  "will be wating for you and the final stage. Defeat him and become KING OF ALL THE LANDS")
