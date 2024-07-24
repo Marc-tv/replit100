1d4ff96b5fbea778729a3e0434dce5669edf1f49
Marc-tv <marcvatapilly@gmail.com>
2024-07-24 18:54:05 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,13 @@
+print("Marvel Movie Character Creator")
+print("--")
+question1 = input("Do you like 'hanging around'?:")
+if question1 == "no":
+  print("Then you're not Spider-man")
+
+question2 = input("Do you have a 'gravelly' voice?:")
+if question2 == "no":
+  print("Aww, then you're not Korg")
+
+question3 = input("Do you often feel 'Marvelous'?:")
+if question3 == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
